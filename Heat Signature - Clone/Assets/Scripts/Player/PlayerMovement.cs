2d99cdf8f72a652eff2cd7beabb5dc031e97e6c8
Acriid <wilrichgerdus@gmail.com>
2026-08-09 16:36:53 +0200

    Added Slipstream to player

--- Heat Signature - Clone/Assets/Scripts/Player/PlayerMovement.cs
+++ Heat Signature - Clone/Assets/Scripts/Player/PlayerMovement.cs
@@ -12,19 +12,28 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Rigidbody2D _playerRigidBody = null;
     [SerializeField] private float _playerSpeed = 10f;
     [SerializeField] private InputReader _inputReader = null;
+    [SerializeField] private TimeManager _timeManager = null;
 
 
     bool _slowMotion = false;
+    private float _timeChangeFloat = 1f;
     private void OnEnable()
     {
         if(_inputReader != null)
         EnablePlayerMovement();
+
+
+        if(_timeManager != null)
+        _timeManager.OnPlayerTimeChange += ChangeTimeVariable;
     }
 
     private void OnDisable()
     {
         if(_inputReader != null)
         DisablePlayerMovement();
+
+        if(_timeManager != null)
+        _timeManager.OnPlayerTimeChange -= ChangeTimeVariable;
     }
     void FixedUpdate()
     {
@@ -46,7 +55,7 @@ public class PlayerMovement : MonoBehaviour
 
         if(_slowMotion)
         {
-            _playerRigidBody.AddForce(_playerSpeed * moveInput); 
+            _playerRigidBody.AddForce(_playerSpeed * moveInput * _timeChangeFloat); 
         }
         else
         {
@@ -66,5 +75,29 @@ public class PlayerMovement : MonoBehaviour
         _inputReader.DisableMoveAction();
 
         _inputReader.OnMove -= ReadMoveInput;
+    }
+
+    private void ChangeTimeVariable(float newValue)
+    {
+        _timeChangeFloat = newValue;
+
+        if(_timeChangeFloat > 1f)
+        {
+            _slowMotion = true;
+            
+        }
+        else
+        {
+            StartCoroutine(SlipstreamBurst());
+        }
+    }
+
+    private IEnumerator SlipstreamBurst()
+    {
+        if(_playerRigidBody != null)
+        _playerRigidBody.AddForce(new Vector2(_playerRigidBody.linearVelocityX,_playerRigidBody.linearVelocityY) * _playerSpeed / 2f,ForceMode2D.Impulse);
+
+        yield return new WaitForSeconds(0.2f);
+        _slowMotion = false;
     }
 }
