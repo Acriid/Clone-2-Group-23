fba4eef001e8d4c932c4bd963627d8fdb989b121
Acriid <wilrichgerdus@gmail.com>
2026-08-09 10:36:14 +0200

    Removed bloat and organized files

---
+++ Heat Signature - Clone/Assets/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    //Public Variables
+    public Transform Player;
+
+    //Adjusts Camera 
+    void LateUpdate()
+    {
+        Vector3 cameraPosition = Player.position;
+        cameraPosition.z = 10f;
+        transform.position = cameraPosition;
+    }
+}
